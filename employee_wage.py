71c20454531a796135846bf5b65ad047cf2475ed
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-09 16:42:05 +0530

    Added : Code to calculate Employee Wage

--- employee_wage.py
+++ employee_wage.py
@@ -1,8 +1,13 @@
 import random
 
+wage_per_hour = 20
 print("Welcome to Employee Wage Computation Program")
 attendance = random.randint(0, 1)
 if attendance == 0:
     print("Employee is present")
+    full_day = 8
 else:
     print("Employee is absent")
+    full_day = 0
+wage = wage_per_hour * full_day
+print("Employee wage ", wage)
