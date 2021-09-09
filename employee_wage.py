f24519134461a9dbab1b52ec6a0b097521737385
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-09 16:45:14 +0530

    Added : Code to check part time

--- employee_wage.py
+++ employee_wage.py
@@ -1,13 +1,15 @@
 import random
 
 wage_per_hour = 20
-print("Welcome to Employee Wage Computation Program")
-attendance = random.randint(0, 1)
+attendance = random.randint(0, 2)
 if attendance == 0:
-    print("Employee is present")
-    full_day = 8
+    print("Employee worked full time")
+    hours_worked = 8
+elif attendance == 1:
+    print("Employee worked part time")
+    hours_worked = 4
 else:
-    print("Employee is absent")
-    full_day = 0
-wage = wage_per_hour * full_day
+    print("Employee is Absent")
+    hours_worked = 0
+wage = wage_per_hour * hours_worked
 print("Employee wage ", wage)
