4577ff59c429195616381d3333f8ef38d82d5bbb
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-09 16:51:24 +0530

    Added : Code to calculate wage till total days worked or total hours worked is reached

--- employee_wage.py
+++ employee_wage.py
@@ -4,6 +4,7 @@ WAGE_PER_HOUR = 20
 FULL_TIME = 8
 PART_TIME = 4
 TOTAL_WORKING_DAYS = 20
+TOTAL_WORKING_HOURS = 100
 
 
 def calculate_wage():
@@ -16,10 +17,12 @@ def calculate_wage():
             0
     }
     days_worked = 0
-    while days_worked < TOTAL_WORKING_DAYS:
+    hours_worked = 0
+    while days_worked < TOTAL_WORKING_DAYS and hours_worked < TOTAL_WORKING_HOURS :
         attendance = random.randint(0, 2)
-        days_worked = days_worked + emp_attendance.get(attendance)
-    return WAGE_PER_HOUR * days_worked
+        hours_worked = hours_worked + emp_attendance.get(attendance)
+
+    return WAGE_PER_HOUR * hours_worked
 
 
 total_wage = calculate_wage()
