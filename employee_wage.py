b865eb4c3b4039164a501fe0edd1f642bad8a62f
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-11 12:45:48 +0530

    Refactored : To use class

--- employee_wage.py
+++ employee_wage.py
@@ -7,18 +7,16 @@ TOTAL_WORKING_DAYS = 20
 TOTAL_WORKING_HOURS = 100
 
 
-def calculate_wage():
-    emp_attendance = {
-        0:
-            0,
-        1:
-            FULL_TIME,
-        2:
-            PART_TIME
-    }
-
-    if __name__ == "__main__":
-        main()
+class Employee:
+    def calculate_wage(self):
+        emp_attendance = {
+            0:
+                0,
+            1:
+                FULL_TIME,
+            2:
+                PART_TIME
+        }
         days_worked = 0
         hours_worked = 0
         while days_worked < TOTAL_WORKING_DAYS and hours_worked < TOTAL_WORKING_HOURS:
@@ -27,9 +25,13 @@ def calculate_wage():
                 hours_worked = hours_worked + emp_attendance.get(attendance)
                 days_worked = days_worked + 1
             except:
-                print("Please enter a valid input")
+                print("Please enter a valid input !!!")
         return WAGE_PER_HOUR * hours_worked
 
 
-total_wage = calculate_wage()
+emp = Employee()
+
+if __name__ == "__main__":
+    main()
+total_wage = emp.calculate_wage()
 print("Total employee wage ", total_wage)
