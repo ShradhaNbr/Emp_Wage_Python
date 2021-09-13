5755a710d31af4ffe12ca0ee605faa9e593d79bf
ShradhaNbr <85601322+ShradhaNbr@users.noreply.github.com>
2021-09-13 15:21:37 +0530

    Refactored : Code to add custom exception

--- employee_wage.py
+++ employee_wage.py
@@ -1,12 +1,15 @@
+import random
+
+from exception import ValueTooLargeError, ValueTooSmallError
+
 WAGE_PER_HOUR = 20
 FULL_TIME = 8
 PART_TIME = 4
-TOTAL_WORKING_DAYS = 20
-TOTAL_WORKING_HOURS = 100
 
 
 class Employee:
-    def calculate_wage(self):
+    @staticmethod
+    def calculate_wage():
         emp_attendance = {
             0:
                 0,
@@ -17,13 +20,23 @@ class Employee:
         }
         days_worked = 0
         hours_worked = 0
-        while days_worked < TOTAL_WORKING_DAYS and hours_worked < TOTAL_WORKING_HOURS:
-            try:
-                attendance = int(input("Enter the attendance 0.Absent 1.Full- time 2. Part-time \n"))
+        try:
+            total_working_days = int(input("Enter total number of working days\n"))
+            total_working_hours = int(input("Enter total number of working hours\n"))
+            if total_working_days > 30 or total_working_hours > 250:
+                raise ValueTooLargeError
+            elif total_working_days < 5 or total_working_hours < 50:
+                raise ValueTooSmallError
+            while days_worked < total_working_days and hours_worked < total_working_hours:
+                attendance = random.randint(0, 2)
                 hours_worked = hours_worked + emp_attendance.get(attendance)
                 days_worked = days_worked + 1
-            except:
-                print("Please enter a valid input !!!")
+        except ValueError:
+            print("Please enter a valid input !!!")
+        except ValueTooLargeError:
+            print("Value is too large. Please enter a smaller value")
+        except ValueTooSmallError:
+            print("Value is too small. Please enter a larger value")
         return WAGE_PER_HOUR * hours_worked
 
 
