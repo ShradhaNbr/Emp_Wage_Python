83128c3d2b0abaeafdff721096832a0a4285dbed
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-13 12:03:12 +0530

    Refactored : Code to add custom exception

---
+++ exception.py
@@ -0,0 +1,8 @@
+class ValueTooLargeError(Exception):
+    """Raised when input is too large"""
+    pass
+
+
+class ValueTooSmallError(Exception):
+    """Raised when input is too small"""
+    pass
