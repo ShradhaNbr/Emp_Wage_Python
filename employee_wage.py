39c50875e1442a376c10683b70c06226fdf864b2
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-14 16:17:07 +0530

    Refactored : Code to use loggers

--- employee_wage.py
+++ employee_wage.py
@@ -1,12 +1,20 @@
 import random
-
+import logging
 from exception import ValueTooLargeError, ValueTooSmallError
+
+logging.basicConfig(
+    filename = "employee_wage.log",
+    filemode = 'w',
+    level=logging.INFO
+)
+log = logging.getLogger()
 
 class Employee:
 
-    def __init__(self,wage_per_hour):
+
+    def __init__(self,wage_per_hour,company):
         self.wage_per_hour=wage_per_hour
-        # self.company = company
+        self.company = company
 
     def calculate_wage(self):
         FULL_TIME = 8
@@ -33,15 +41,17 @@ class Employee:
                 hours_worked = hours_worked + emp_attendance.get(attendance)
                 days_worked = days_worked + 1
                 total_wage = self.wage_per_hour * hours_worked
-            print("Total employee wage ", total_wage)
+            print("Total employee wage for ", self.company, "is", total_wage)
         except ValueError:
-            print("Please enter a valid input !!!")
+            log.error("Please enter a valid input !!!")
         except ValueTooLargeError:
-            print("Value is too large. Please enter a smaller value")
+            log.warning("Value is too large. Please enter a smaller value")
         except ValueTooSmallError:
-            print("Value is too small. Please enter a larger value")
+            log.warning("Value is too small. Please enter a larger value")
 
 if __name__ == "__main__":
-    emp = Employee(20)
-    emp.calculate_wage()
+    company1 = Employee(30,"TCS")
+    company2 = Employee(20, "Infosys")
+    company1.calculate_wage()
+    company2.calculate_wage()
 
